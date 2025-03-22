d012169c643552135f834e3259e5711075d7e16d
jathin12e <126076303+jathin12e@users.noreply.github.com>
2025-03-23 00:59:48 +0530

    secont commit

--- check.py
+++ check.py
@@ -134,4 +134,4 @@ with gr.Blocks() as demo:
 
 # Run the Gradio app
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 8080)))
